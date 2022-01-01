f5fa437ce6bccb440f6edc2dfac02cca26b5969f
Archit Garg <thearchitgargg@gmail.com>
2022-01-01 14:41:43 +0530

    feat: add hamburger component

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,4 @@ import "./global.scss";
 export * from "./components/ShiningDiv/ShiningDiv";
 export * from "./components/Button/Button";
 export * from "./components/Footer/Footer";
+export * from "./components/Hamburger/Hamburger";
