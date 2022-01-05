f7f1b90ef670238d92d9f550580391f7d321b3c4
Archit Garg <thearchitgargg@gmail.com>
2022-01-06 00:55:35 +0530

    chore: add code in example dir

--- example/index.tsx
+++ example/index.tsx
@@ -2,12 +2,41 @@ import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { ShiningDiv } from "react-landing-ui";
 import "./indec.css";
+import { Button, Footer, NavBar, ShiningDiv, Switch } from "react-landing-ui";
 
 const App = () => {
   return (
-    <ShiningDiv className="main">Test</ShiningDiv>
+    <>
+      <NavBar logo={<span className="logo">Logo</span>}>
+        <a href="#">About</a>
+        <a href="#">Pricing</a>
+        <a href="#">Roadmap</a>
+        <a href="#">Contact</a>
+        <ShiningDiv>
+          <a target="_blank" href="https://github.com/architgarg/react-landing-ui/">
+            <Button>View on Github</Button>
+          </a>
+        </ShiningDiv>
+      </NavBar>
+      <div className="main">
+        <Switch label="Sample Switch"/>
+      </div>
+      <Footer
+        logo={<span className="logo">Logo</span>}
+        mainLinks={<>
+          <a href="#">About</a>
+          <a href="#">Pricing</a>
+          <a href="#">Roadmap</a>
+          <a href="#">Contact</a>
+        </>}
+        extraLinks={<>
+          <a href="#">Privacy</a>
+          <a href="#"> &copy; Archit 2022</a>
+        </>}
+      />
+    </>
+
   );
 };
 
